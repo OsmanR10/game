c2c8164b67e029ce999e30fbd5e210f2ff904813
OsmanR10 <ozzyreyrey@gmail.com>
2017-06-01 10:28:58 -0600

    Initial Commit

---
+++ temp/BackupAssets/assets/scripts/Star.js
@@ -0,0 +1,49 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        //When the distance between the star and main character is less than this value,
+        //collection os the point will be completed
+        pickRadius: 0
+    },
+
+    // use this for initialization
+    onLoad: function () {
+
+    },
+    
+    getPlayerDistance: function(){
+        //judge the distance according to the position of the player node
+        var playerPos = this.game.player.getPosition();
+        //calculate the distance between two nodes according to their positions
+        var dist = cc.pDistance(this.node.position, playerPos);
+        return dist;
+    },
+    
+    onPicked: function(){
+        //when the starts are being collected, invoke the interface in the game
+        //script to generate a new star
+        this.game.spawnNewStar();
+        //invoke the scoring method of the Game script
+        this.game.gainScore();
+        //then destroy the surrent star's node
+        this.node.destroy();
+    },
+
+    // called every frame, uncomment this function to activate update callback
+    update: function (dt) {
+        //judge is the distance between the star and main character is shoter
+        //then the collecting distance for each frame
+        if(this.getPlayerDistance() < this.pickRadius){
+            //invoke collecting behavior
+            this.onPicked();
+            return;
+        }
+        
+        //update the transparency of the star according to the timer int the
+        //Game script
+        var opacityRation = 1 - this.game.timer/this.game.starDuration;
+        var minOpacity = 50;
+        this.node.opacity = minOpacity + Math.floor(opacityRation * (255 - minOpacity));
+    },
+});
---
+++ temp/scripts/assets/scripts/Star.js
@@ -0,0 +1,55 @@
+"use strict";
+cc._RF.push(module, '344841Je7lBmrN0d8Dx0nl6', 'Star');
+// scripts\Star.js
+
+"use strict";
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        //When the distance between the star and main character is less than this value,
+        //collection os the point will be completed
+        pickRadius: 0
+    },
+
+    // use this for initialization
+    onLoad: function onLoad() {},
+
+    getPlayerDistance: function getPlayerDistance() {
+        //judge the distance according to the position of the player node
+        var playerPos = this.game.player.getPosition();
+        //calculate the distance between two nodes according to their positions
+        var dist = cc.pDistance(this.node.position, playerPos);
+        return dist;
+    },
+
+    onPicked: function onPicked() {
+        //when the starts are being collected, invoke the interface in the game
+        //script to generate a new star
+        this.game.spawnNewStar();
+        //invoke the scoring method of the Game script
+        this.game.gainScore();
+        //then destroy the surrent star's node
+        this.node.destroy();
+    },
+
+    // called every frame, uncomment this function to activate update callback
+    update: function update(dt) {
+        //judge is the distance between the star and main character is shoter
+        //then the collecting distance for each frame
+        if (this.getPlayerDistance() < this.pickRadius) {
+            //invoke collecting behavior
+            this.onPicked();
+            return;
+        }
+
+        //update the transparency of the star according to the timer int the
+        //Game script
+        var opacityRation = 1 - this.game.timer / this.game.starDuration;
+        var minOpacity = 50;
+        this.node.opacity = minOpacity + Math.floor(opacityRation * (255 - minOpacity));
+    }
+});
+
+cc._RF.pop();
